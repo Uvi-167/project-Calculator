58c8d40bfea07fa5e26778233510acbb689b7907
Yuvashree T <117257752+Uvi-167@users.noreply.github.com>
2023-07-01 14:11:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+const output = document.getElementById("Output-value");
+
+function display(num){
+    output.value += num;
+}
+
+function clearBtn(){
+    output.value ="";
+}
+function deleteBtn(){
+    output.value =output.value.slice(0,-1);
+}
+
+function calculate(){
+    try {
+        output.value = eval(output.value);
+    } catch (err) {
+        alert("Invalid");
+    }
+}
+function sin_Btn(){
+       output.value= Math.sin(output.value);
+    }
+
+    function cos_Btn()
+    {
+        output.value=Math.cos(output.value);
+    }
+
+    function tan_Btn()
+    {
+        output.value=Math.tan(output.value);
+    }
+
+    function pow_Btn()
+    {
+        output.value=Math.pow(output.value,2);
+    }
+
+    function sqrt_Btn()
+    {
+        output.value=Math.sqrt(output.value,2);
+    }
+
+    function log_Btn()
+    {
+        output.value=Math.log(output.value);
+    }
+
+    function pi_Btn()
+    {
+        output.value= 3.14159265359;
+    }
+
+    function e_Btn()
+    {
+        output.value=2.71828182846;
+    }
+
