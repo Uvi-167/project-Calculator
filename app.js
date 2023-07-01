3621f7d2bd33192b313550cfe8ca22c95ab3b3a6
Yuvashree T <117257752+Uvi-167@users.noreply.github.com>
2023-07-01 15:57:20 +0530

    app.js

--- app.js
+++ app.js
@@ -1,80 +1,72 @@
 const output = document.getElementById("Output-value");
-
 window.addEventListener("keypress",e=>{
     if (e.code === 'Tab'){
         output.focus();        
 }});
 
-function display(num){
+function display(num) {
     output.value += num;
 }
 
-function clearBtn(){
-    output.value ="";
-}
-function deleteBtn(){
-    output.value =output.value.slice(0,-1);
+function clearBtn() {
+    output.value = "";
 }
 
-function calculate(){
+
+window.addEventListener("keypress", e => {
+    if (e.code === 'Delete') {
+        clearBtn();
+    }
+});
+
+
+function deleteBtn() {
+    output.value = output.value.slice(0, -1);
+}
+
+
+function calculate() {    
     try {
         output.value = eval(output.value);
     } catch (err) {
         alert("Invalid");
     }
 }
-function sin_Btn(){
-       output.value= Math.sin(output.value);
-    }
-
-    function cos_Btn()
-    {
-        output.value=Math.cos(output.value);
-    }
-
-    function tan_Btn()
-    {
-        output.value=Math.tan(output.value);
-    }
-
-    function pow_Btn()
-    {
-        output.value=Math.pow(output.value,2);
-    }
-
-    function sqrt_Btn()
-    {
-        output.value=Math.sqrt(output.value,2);
-    }
-
-    function log_Btn()
-    {
-        output.value=Math.log(output.value);
-    }
-
-    function pi_Btn()
-    {
-        output.value= 3.14159265359;
-    }
-
-    function e_Btn()
-    {
-        output.value=2.71828182846;
-    }
 
 window.addEventListener("keypress",e=>{
-    if (e.code === 'NumpadEnter' || e.key === 'Enter'){
+    if (e.code === 'NumpadEnter'){
         calculate();
         
 }});
 
-window.addEventListener("keypress",e=>{
-    if (e.code === 'Delete' || e.key ===  'Delete'){
-        clearBtn();
-        
-}});
+function sin_Btn() {
+    output.value = Math.sin(output.value);
+}
 
-window.addEventListener("keypress",e=>{
-    if (e.code === 'Backspace'){
-        deleteBtn();        
-}});
+function cos_Btn() {
+    output.value = Math.cos(output.value);
+}
+
+function tan_Btn() {
+    output.value = Math.tan(output.value);
+}
+
+function pow_Btn() {
+    output.value = Math.pow(output.value, 2);
+}
+
+function sqrt_Btn() {
+    output.value = Math.sqrt(output.value, 2);
+}
+
+function log_Btn() {
+    output.value = Math.log(output.value);
+}
+
+function pi_Btn() {
+    output.value = 3.14159265359;
+}
+
+function e_Btn() {
+    output.value = 2.71828182846;
+}
